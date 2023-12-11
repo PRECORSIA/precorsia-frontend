b25bc5984483889e208ed7791d38871a72fc5c98
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-12-11 20:01:47 -0300

    added a base test file to check badge

---
+++ test/build.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('---- basic Build ----', () => {
+
+    it('This test does nothing, just lets the build pass', () => {
+        expect(1).to.be.equal(1);
+    });
+
+});
