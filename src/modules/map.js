26d067d894df8a1aa2e12f7671532fc5de483b67
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-12-11 21:12:00 -0300

    fixed too large areas problem

--- src/modules/map.js
+++ src/modules/map.js
@@ -1,21 +1,25 @@
-import { 
-  area as _area, 
-  bbox as _bbox, 
-  bboxPolygon as _bboxPolygon, 
-  centroid, 
-  polygon as _polygon, 
-  squareGrid 
+import {
+    area as _area,
+    bbox as _bbox,
+    bboxPolygon as _bboxPolygon,
+    centroid,
+    polygon as _polygon,
+    squareGrid
 } from '@turf/turf';
 
 function segment(points, target_area) {
-    
+
     const polygon = _polygon([points]);
     const bbox = _bbox(polygon);
+
+    const polygonArea = _area(polygon);
+    if (polygonArea > 3774873.7) {
+        return [];
+    }
 
     const cellSize = target_area / 1000;
     const grid = squareGrid(bbox, cellSize, { units: 'kilometers' });
 
-    // Find the centroid of each cell and store them in an array
     const centroids = grid.features.map(cell => centroid(cell));
     return centroids;
 }
