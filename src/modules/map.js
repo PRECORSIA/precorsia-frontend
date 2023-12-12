945c6a308de0f151e412415a6636f47f56765570
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-12-11 21:14:08 -0300

    passing too few points test

--- src/modules/map.js
+++ src/modules/map.js
@@ -8,14 +8,18 @@ import {
 } from '@turf/turf';
 
 function segment(points, target_area) {
-
-    const polygon = _polygon([points]);
-    const bbox = _bbox(polygon);
-
+    
+    if (points.length < 4) {
+        return [];
+    }
+    
     const polygonArea = _area(polygon);
     if (polygonArea > 3774873.7) {
         return [];
     }
+
+    const polygon = _polygon([points]);
+    const bbox = _bbox(polygon);
 
     const cellSize = target_area / 1000;
     const grid = squareGrid(bbox, cellSize, { units: 'kilometers' });
