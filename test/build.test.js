620fd099433c3f4c151805d88addac1234736e4f
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-12-11 21:12:10 -0300

    removed basic build test

--- test/build.test.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-
-describe('---- basic Build ----', () => {
-
-    it('This test does nothing, just lets the build pass', () => {
-        expect(1).to.be.equal(1);
-    });
-
-});
